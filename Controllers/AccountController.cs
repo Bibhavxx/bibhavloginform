9e5c6ecd6d6d54e1ea9c8ac3a63748ca48d861b9
Bibhav Kumar Dubey <134066370+Bibhavxx@users.noreply.github.com>
2025-07-01 15:24:39 +0530

    Add files via upload

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Login_App.Models;
+using LoginApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login_App.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
+        {
+            if (user == null)
+            {
+                ViewBag.Message = "User data is missing.";
+                return View();
+            }
+
+            var users = new List<User>
+            {
+                new User { Username = "aadarsh", Password = "aadarsh12" },
+                new User { Username = "bibhav", Password = "bibhavdubey12" },
+                new User { Username = "aaditya", Password = "aaditya12" }
+            };
+
+            var matchedUser = users.FirstOrDefault(u =>
+                u.Username == user.Username && u.Password == user.Password);
+
+            if (matchedUser != null)
+            {
+                ViewBag.Username = matchedUser.Username;
+                return View("Welcome");
+            }
+
+            ViewBag.Message = "Invalid Username or Password";
+            return View();
+        }
+    }
+}
